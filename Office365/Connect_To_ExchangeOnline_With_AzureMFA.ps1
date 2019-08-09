5f2c2e2a49899c5fb07d4bbeabca8a16dc25f694
Morten Pedholt <mortenpedersen95@hotmail.com>
2019-08-09 11:02:54 +0200

    MFA change

--- Office365/Connect_To_ExchangeOnline_With_AzureMFA.ps1
+++ Office365/Connect_To_ExchangeOnline_With_AzureMFA.ps1
@@ -1,4 +1,4 @@
-﻿## connect til exchange online med MFA
+﻿# connect til exchange online med MFA
 Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
 $MFCCPSSession = New-ExoPSSession -ConnectionUri 'https://outlook.office365.com/powershell-liveid'
 import-pssession $MFCCPSSession
