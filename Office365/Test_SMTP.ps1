518b5da04c0d1e3a6cc9c7aba30c7ff32b1a2e2f
Morten Pedholt <mortenpedersen95@hotmail.com>
2019-08-09 11:02:18 +0200

    Add text

--- Office365/Test_SMTP.ps1
+++ Office365/Test_SMTP.ps1
@@ -1,1 +1,3 @@
+#Test Office365 SMTP.
+#You must use the same user login as the "from" user'
 Send-MailMessage -SmtpServer smtp.office365.com -port 587 -To user@domain.dk -From user@domain.dk -Body "test" -Subject "test" -Credential $cred -UseSsl
