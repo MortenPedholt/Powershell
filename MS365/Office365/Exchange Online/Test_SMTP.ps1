881a41e161441f1528f9ecee0e05a73acf0c5448
Morten Pedholt Pedersen <moped@itrelation.dk>
2019-09-25 13:14:20 +0200

    smtp modification

--- MS365/Office365/Exchange Online/Test_SMTP.ps1
+++ MS365/Office365/Exchange Online/Test_SMTP.ps1
@@ -2,5 +2,5 @@
 #You must use the same user login as the "from" user'
 Send-MailMessage -SmtpServer smtp.office365.com -port 587 -To user@domain.dk -From user@domain.dk -Body "test" -Subject "test" -Credential $cred -UseSsl
 
-
-Send-MailMessage -SmtpServer bentbrandt-dk.mail.protection.outlook.com -port 25 -To moped@itrelation.dk -From test@bentbrandt.dk -Body "test" -Subject "test"
+#
+Send-MailMessage -SmtpServer domain.mail.protection.outlook.com -port 25 -To moped@domain.dk -From test@domain.dk -Body "test" -Subject "test"
