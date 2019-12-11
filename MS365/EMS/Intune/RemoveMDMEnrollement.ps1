c5b256f94c55d52b9be057978822bcc9f0acee55
Morten Pedholt Pedersen <moped@itrelation.dk>
2019-12-11 08:45:02 +0100

    Remove MDM Enrollment

--- MS365/EMS/Intune/RemoveMDMEnrollement.ps1
+++ MS365/EMS/Intune/RemoveMDMEnrollement.ps1
@@ -1,13 +1,23 @@
 $MDMEnrollment = "https://enrollment.manage.microsoft.com/enrollmentserver/discovery.svc"
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Enrollments\"
- 
-foreach ($CurrentPath in $RegPath) { 
-    Get-ChildItem $CurrentPath -Recurse |  
+
+foreach ($Path in $RegPath) { 
+    Get-ChildItem $Path -Recurse |
     ForEach-Object { 
         $Key = $_
-        if (($Key.GetValueNames() | % { $Key.GetValue($_) }) -eq $MDMEnrollment) {  
+       $CheckKeyPath = if (($Key.GetValueNames() | % { $Key.GetValue($_) }) -eq $MDMEnrollment) {  
             Write-Host $Key
+            
             Remove-Item -Path Registry::$Key -Recurse
+            
+                       
+            if ($CheckKeyPath) {
+            Write-Host "Not every key have been deleted, continue to remove keys" -ForegroundColor Cyan }
+            else
+            { Write-Host "$Key has been deleted, ending foreach loop" -ForegroundColor Cyan
+              Break
+            }
         } 
     } 
 }
+
