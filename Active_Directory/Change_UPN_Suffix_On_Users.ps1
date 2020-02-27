a56ec623e040213b31d3e26f61defc2148ae0ab1
MortenPedholt <45897453+MortenPedholt@users.noreply.github.com>
2020-02-27 21:47:23 +0100

    Update Change_UPN_Suffix_On_Users.ps1

--- Active_Directory/Change_UPN_Suffix_On_Users.ps1
+++ Active_Directory/Change_UPN_Suffix_On_Users.ps1
@@ -1,10 +1,10 @@
 #
 #Change UPN Suffix on AD Users.
 #
-$oldSuffix = "@Scandistandard.com"
-$newSuffix = "@naapurinmaalaiskana.fi"
+$oldSuffix = "@olddomain.com"
+$newSuffix = "@newdomain.com"
 $ou = "OU=Users,DC=domain,DC=local"
-$server = "DKSCANARSDC2"
+$server = "servername"
 Get-ADUser -identity $ou| ForEach-Object {
 $newUpn = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
 $_ | Set-ADUser -Server $server -UserPrincipalName $newUpn
