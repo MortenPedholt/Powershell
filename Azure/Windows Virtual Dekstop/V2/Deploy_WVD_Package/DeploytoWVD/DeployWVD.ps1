af7f1264101bf0bc97bc787d2181cf25dcebeed2
MortenPedholt <mortenpedersen95@hotmail.com>
2020-04-23 16:07:50 +0200

    WVD DeploymentContent

---
+++ Azure/Windows Virtual Dekstop/V2/Deploy_WVD_Package/DeploytoWVD/DeployWVD.ps1
@@ -0,0 +1,80 @@
+﻿#Check if path exist, else create
+$OutputPath = "C:\DeployWVD\"
+$StorageAccountName = ""
+
+$path = $OutputPath
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+
+$Logpath = $OutputPath
+start-Transcript -Path "$Logpath\DeployWVD_Log_$(get-date -f yyyy-MM-dd).txt" -IncludeInvocationHeader -Append -Force -Verbose
+
+#PARAMETERS
+Write-Host "Reading parameters..." -ForegroundColor Cyan
+$SourceURL = "https://$StorageAccountName.blob.core.windows.net/dev/DeploytoWVD/DeploymentContent/"
+
+$AgentScript = "InstallAgents.ps1"
+$RDBootLoader = "Microsoft.RDInfra.RDAgentBootLoader.Installer-x64.msi"
+$RDAgent = "Microsoft.RDInfra.RDAgent.Installer-x64-1.0.1288.2700.msi"
+$Registrationkey = "RegistrationKey.txt"
+$CheckVariableScript = "CheckSystemVariables.ps1"
+
+#Download Agentscript
+Write-Host "Downloading Agent Script..." -ForegroundColor Cyan
+
+$url = $SourceURL + $AgentScript
+$output = $OutputPath + $AgentScript
+
+Invoke-WebRequest -Uri $url -OutFile $output -Verbose
+
+#Download CheckVariableScript
+Write-Host "Downloading Check variable Script..." -ForegroundColor Cyan
+
+$url = $SourceURL + $CheckVariableScript
+$output = $OutputPath + $CheckVariableScript
+
+Invoke-WebRequest -Uri $url -OutFile $output -Verbose
+
+
+#Download RD Boot Loader
+Write-Host "Downloading RD Boot Loader.." -ForegroundColor Cyan
+
+$url = $SourceURL + $RDBootLoader
+$output = $OutputPath + $RDBootLoader
+
+Invoke-WebRequest -Uri $url -OutFile $output -Verbose
+
+#Download RD Agent
+Write-Host "Downloading RD Agent..." -ForegroundColor Cyan -Verbose
+
+$url = $SourceURL + $RDAgent
+$output = $OutputPath + $RDAgent
+
+Invoke-WebRequest -Uri $url -OutFile $output -Verbose
+
+#Download WVD Regristration Key
+Write-Host "Downloading WVD RegistrationK Key..." -ForegroundColor Cyan
+
+$url = $SourceURL + $Registrationkey
+$output = $OutputPath + $Registrationkey
+
+Invoke-WebRequest -Uri $url -OutFile $output -Verbose
+
+#Execute AgentDeployment
+
+Invoke-Expression $OutputPath$AgentScript -Verbose
+Write-Host "Executing script $AgentScript..." -ForegroundColor Cyan
+
+Write-Host "End of script" -ForegroundColor Cyan
+
+#Execute CheckVariableScript
+
+Invoke-Expression $OutputPath$CheckVariableScript -Verbose
+Write-Host "Executing script $CheckVariableScript..." -ForegroundColor Cyan
+
+Write-Host "End of script" -ForegroundColor Cyan
+
+Stop-Transcript
---
+++ Azure/Windows Virtual Dekstop/V2/Deploy_WVD_Package/DeploytoWVD/DeploymentContent/CheckSystemVariables.ps1
@@ -0,0 +1,364 @@
+﻿$Executionpath = "C:\DeployWVD\"
+start-Transcript -Path "$Executionpath\CheckSystemVariable_Log_$(get-date -f yyyy-MM-dd).txt" -IncludeInvocationHeader -Append -Force -Verbose
+
+$Hostpoolname = ""
+$HostpoolResourcegroup = ""
+
+$Hostname = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
+
+#$MONITORING_AGENT_CLUSTER
+$VariableName = "MONITORING_AGENT_CLUSTER"
+$value = "EUS2R0C100"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_AGENT_RING
+$VariableName = "MONITORING_AGENT_RING"
+$value = "R0"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+#$MONITORING_CONFIG_VERSION
+$VariableName = "MONITORING_CONFIG_VERSION"
+$value = "1.3"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+
+#$MONITORING_DATA_DIRECTORY
+$VariableName = "MONITORING_DATA_DIRECTORY"
+$value = "C:\windows\system32\config\systemprofile\AppData\Roaming\Microsoft\Monitoring"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_ENV
+$VariableName = "MONITORING_ENV"
+$value = "PROD"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_ACCOUNT
+$VariableName = "MONITORING_GCS_ACCOUNT"
+$value = "RDSAgentProd"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_AUTH_ID
+$VariableName = "MONITORING_GCS_AUTH_ID"
+$value = "RDSAgentPROD.geneva.keyvault.RDSAGENT.WVD"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_AUTH_ID_TYPE
+$VariableName = "MONITORING_GCS_AUTH_ID_TYPE"
+$value = "AuthKeyVault"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_CERTSTORE
+$VariableName = "MONITORING_GCS_CERTSTORE"
+$value = "LOCAL_MACHINE\MY"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_ENVIRONMENT
+$VariableName = "MONITORING_GCS_ENVIRONMENT"
+$value = "Diagnostics Prod"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_NAMESPACE
+$VariableName = "MONITORING_GCS_NAMESPACE"
+$value = "RDSAgentProd"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+#$MONITORING_GCS_REGION
+$VariableName = "MONITORING_GCS_REGION"
+$value = "westeurope"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+
+#$MONITORING_MDM_ACCOUNT
+$VariableName = "MONITORING_MDM_ACCOUNT"
+$value = "RDSAgentProd"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+
+
+
+#$MONITORING_TENANT
+$VariableName = "MONITORING_TENANT"
+$value = "PROD"
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+#$MONITORING_RESOURCE
+$VariableName = "MONITORING_RESOURCE"
+$value = $HostpoolResourcegroup
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+
+#$MONITORING_ROLE
+$VariableName = "MONITORING_ROLE"
+$value = $Hostpoolname
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+#$MONITORING_ROLE_INSTANCE
+$VariableName = "MONITORING_ROLE_INSTANCE"
+$value = $Hostname
+$Type = "Machine"
+$Checkvariable = [Environment]::GetEnvironmentVariable($VariableName, $Type)
+
+Write-Host "Checking enironment variable $VariableName .." -ForegroundColor Cyan
+
+if (-not $Checkvariable){
+
+Write-Host "Enironment variable is not set, setting variable $VariableName .." -ForegroundColor Cyan
+[Environment]::SetEnvironmentVariable($VariableName, $value, $Type)
+
+}else {
+
+Write-Host "Enironment variable for $VariableName is already set" -ForegroundColor Cyan
+
+}
+
+
+$TaskName = "GenevaTask"
+Write-Host "Checking if GenevaTask schedule task is created"
+$GetTask = Get-ScheduledTask | Where-Object { $_.TaskName -like "$($TaskName)*" }
+if ($GetTask) {
+    Write-Host "Found Schedule Task job $($GetTask).TaskName" -ForegroundColor Cyan
+    Write-Host "Startin Task $($GetTask).TaskName" -ForegroundColor Cyan
+    Start-ScheduledTask -TaskName $GetTask.TaskName
+
+}else{
+
+    Write-Host "Could not find any task named $TaskName" -ForegroundColor Cyan
+
+
+}
+
+Write-Host "End of script" -ForegroundColor Cyan
+
+
+Stop-Transcript
